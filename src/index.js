45d8379521ddec6f87d182b5809115516f39d45f
Nataly-Naf <nat.spivak84@gmail.com>
2023-08-24 12:56:10 +0100

    error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import { fetchBreeds, fetchCatByBreed } from "./js/cat-api";
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
+const { Notify } = require("notiflix");
  const refs = {
     select: document.querySelector('.breed-select'),
-    descr: document.querySelector('.cat-info')
+     descr: document.querySelector('.cat-info'),
+     loader: document.querySelector('.loader'),
+    error: document.querySelector ('.error')
 };
 
 // fetchBreeds()
@@ -27,7 +30,8 @@ fetchBreeds()
                   return `<option value="${breed.id}">${breed.name}</option>`
              })
              .join("");
-             refs.select.innerHTML = markup;
+         refs.select.innerHTML = markup;
+         refs.loader.style.display = "none"
    }
 
 refs.select.addEventListener('change', onOptionSubmit);
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,5 +1,6 @@
 
 import { refs } from ".."
+const { Notify } = require("notiflix");
 
 
 
@@ -16,9 +17,9 @@ export function fetchBreeds() {
         // .then(response => { return response.json() })
         // .then((data) => { (consol.log(data)) }
         .then((resp) => {
-        //    if (!resp.Authenticated) {
-        //     throw new Error('statusText')
-        //     }
+                      if (!resp.ok) {
+             ( Notify.failure(`${resp.statusText}`))
+            }
             return resp.json()
         })
     
@@ -28,13 +29,23 @@ export function fetchCatByBreed(breedId) {
     const breedIds = breedId;
     const breedUrl = 'https://api.thecatapi.com/v1/images/search'
     return fetch(`https://api.thecatapi.com/v1/images/search?api_key=live_dbDbJqnTvZr0BDH9Co8mAYy9oHsrrkDXPCQwO90tvsGws3MQGSe2ul7QIaHBeS63&breed_ids=${breedId}`)
-     .then((resp) => {
-        //    if (!resp.Authenticated) {
-        //     throw new Error(resp.statusText)
-        //     }
-            return (resp.json())
+        .then((resp) => {
+              if (!resp.ok) {
+             ( Notify.failure(`${resp.statusText}`))
+            }
+            return resp.json()
      })
    
 }
 
-
+function createMarkup(arr) {
+    
+    return arr.map((element)=>`
+      <img src="${element.url}" alt="" width="200" height />
+      <div class="cat-descr"> <h2 class="descr-name">${element.breeds[0].name}</h2>
+        <h3 class="descr-text">${element.breeds[0].description}</h3>
+    </div>` ).join('')
+           
+      
+   
+}
